746097917e2dbf348abdec839d1d0105d1838b20
zhouxinlei <zhouxinlei298@163.com>
2020-07-12 22:48:28 +0800

    日志埋点

--- sparksys-commons-log-starter/src/main/java/com/sparksys/commons/log/config/LogConfig.java
+++ sparksys-commons-log-starter/src/main/java/com/sparksys/commons/log/config/LogConfig.java
@@ -17,7 +17,7 @@ public class LogConfig {
 
     @Bean
     @ConditionalOnMissingBean
-    @ConditionalOnExpression("${zuihou.log.enabled:true} && 'LOGGER'.equals('${zuihou.log.type:LOGGER}')")
+    @ConditionalOnExpression("${sparksys.log.enabled:true} && 'LOGGER'.equals('${sparksys.log.type:LOGGER}')")
     public SysLogListener sysLogListener() {
         return new SysLogListener((log) -> PointUtil.debug("0", "OPT_LOG", JSONUtil.parseObj(log).toStringPretty()));
     }
