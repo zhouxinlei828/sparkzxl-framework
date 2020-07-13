850f841ca23ab7d699ccc7ced60117957e0f21a9
zhouxinlei <zhouxinlei298@163.com>
2020-07-13 14:08:02 +0800

    缓存key前缀注释

--- sparksys-commons-core/src/main/java/com/sparksys/commons/core/constant/CacheKey.java
+++ sparksys-commons-core/src/main/java/com/sparksys/commons/core/constant/CacheKey.java
@@ -2,6 +2,12 @@ package com.sparksys.commons.core.constant;
 
 import cn.hutool.core.util.StrUtil;
 
+/**
+ * description: 缓存key前缀
+ *
+ * @author: zhouxinlei
+ * @date: 2020-07-13 14:07:32
+ */
 public class CacheKey {
 
     /**
