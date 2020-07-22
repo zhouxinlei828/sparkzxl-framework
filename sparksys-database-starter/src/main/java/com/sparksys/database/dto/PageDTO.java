f759d31108ec17faa8dcd2201223ad00066c459b
zhouxinlei <zhouxinlei298@163.com>
2020-07-22 09:05:25 +0800

    分页入参

--- sparksys-database-starter/src/main/java/com/sparksys/database/dto/PageDTO.java
+++ sparksys-database-starter/src/main/java/com/sparksys/database/dto/PageDTO.java
@@ -1,8 +1,6 @@
 package com.sparksys.database.dto;
 
-import com.baomidou.mybatisplus.core.metadata.IPage;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
-import lombok.Data;
+import lombok.Getter;
 
 /**
  * description: 分页入参
@@ -10,21 +8,21 @@ import lombok.Data;
  * @author zhouxinlei
  * @date 2020-05-24 13:21:22
  */
-@Data
+@Getter
 public class PageDTO {
 
     /**
      * 当前页
      */
-    private Integer pageNum;
+    private final Integer pageNum;
 
     /**
      * 每页显示条数
      */
-    private Integer pageSize;
+    private final Integer pageSize;
 
-    public <T> IPage<T> startPage() {
-        return new Page<>(pageNum, pageSize);
+    public PageDTO(Integer pageNum, Integer pageSize) {
+        this.pageNum = pageNum == null ? 1 : pageNum;
+        this.pageSize = pageSize == null ? 10 : pageSize;
     }
-
 }
