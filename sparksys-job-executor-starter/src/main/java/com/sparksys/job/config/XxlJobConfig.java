9abacb01b3f545173e41b30b32902b1c48365fc8
zhouxinlei <zhouxinlei298@163.com>
2020-08-21 15:13:59 +0800

    分布式job配置

---
+++ sparksys-job-executor-starter/src/main/java/com/sparksys/job/config/XxlJobConfig.java
@@ -0,0 +1,37 @@
+package com.sparksys.job.config;
+
+import com.sparksys.job.properties.XxlJobProperties;
+import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * description: xxl-job Executor 自动配置
+ *
+ * @author: zhouxinlei
+ * @date: 2020-08-21 14:13:13
+ */
+@Configuration
+@EnableConfigurationProperties(XxlJobProperties.class)
+@Slf4j
+public class XxlJobConfig {
+
+    @Bean
+    public XxlJobSpringExecutor xxlJobExecutor(XxlJobProperties xxlJobProperties) {
+        log.info(">>>>>>>>>>> xxl-job config init.");
+        XxlJobSpringExecutor xxlJobSpringExecutor = new XxlJobSpringExecutor();
+        xxlJobSpringExecutor.setAdminAddresses(xxlJobProperties.getAdminAddresses());
+        xxlJobSpringExecutor.setAppname(xxlJobProperties.getAppName());
+        xxlJobSpringExecutor.setAddress(xxlJobProperties.getAddress());
+        xxlJobSpringExecutor.setIp(xxlJobProperties.getIp());
+        xxlJobSpringExecutor.setPort(xxlJobProperties.getPort());
+        xxlJobSpringExecutor.setAccessToken(xxlJobProperties.getAccessToken());
+        xxlJobSpringExecutor.setLogPath(xxlJobProperties.getLogPath());
+        xxlJobSpringExecutor.setLogRetentionDays(xxlJobProperties.getLogRetentionDays());
+        return xxlJobSpringExecutor;
+    }
+
+
+}
