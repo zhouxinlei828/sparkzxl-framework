33181114ebb9c2b546af1ff7e1318cce55647d8e
zhouxinlei <zhouxinlei298@163.com>
2020-09-13 19:45:06 +0800

    更新sparkzxl-oss-starter

--- sparkzxl-database-starter/src/main/java/com/github/sparkzxl/database/config/MyBatisAutoConfiguration.java
+++ sparkzxl-database-starter/src/main/java/com/github/sparkzxl/database/config/MyBatisAutoConfiguration.java
@@ -2,9 +2,11 @@ package com.github.sparkzxl.database.config;
 
 import cn.hutool.core.lang.Snowflake;
 import cn.hutool.core.util.IdUtil;
+import cn.hutool.json.JSONUtil;
 import com.github.sparkzxl.database.mybatis.hander.MetaDataHandler;
 import com.github.sparkzxl.database.mybatis.injector.BaseSqlInjector;
 import com.github.sparkzxl.database.properties.DataProperties;
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -18,13 +20,22 @@ import org.springframework.context.annotation.Configuration;
  * @date 2020-05-24 13:20:57
  */
 @Configuration
-@EnableConfigurationProperties(DataProperties.class)
-@MapperScan("${mybatis-plus.mapperScan}")
+@EnableConfigurationProperties({DataProperties.class})
+@MapperScan(basePackages = "${mybatis-plus.mapper-scan}")
+@Slf4j
 public class MyBatisAutoConfiguration {
 
+    private final DataProperties dataProperties;
+
+    public MyBatisAutoConfiguration(DataProperties dataProperties) {
+        this.dataProperties = dataProperties;
+        log.info("dataProperties：{}", JSONUtil.toJsonPrettyStr(dataProperties));
+    }
+
     @Bean
-    public Snowflake snowflake(DataProperties dataProperties) {
-        return IdUtil.createSnowflake(dataProperties.getWorkerId(), dataProperties.getDataCenterId());
+    public Snowflake snowflake() {
+        return IdUtil.createSnowflake(dataProperties.getWorkerId(),
+                dataProperties.getDataCenterId());
     }
 
     @Bean
