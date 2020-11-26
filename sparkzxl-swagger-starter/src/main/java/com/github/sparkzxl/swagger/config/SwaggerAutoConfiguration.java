eaa4b7f6a5dde701c72b71a4ef40f6a04da6ad7a
zhouxinlei <zhouxinlei298@163.com>
2020-11-26 17:53:18 +0800

    knife 优化

--- sparkzxl-swagger-starter/src/main/java/com/github/sparkzxl/swagger/config/SwaggerAutoConfiguration.java
+++ sparkzxl-swagger-starter/src/main/java/com/github/sparkzxl/swagger/config/SwaggerAutoConfiguration.java
@@ -3,19 +3,24 @@ package com.github.sparkzxl.swagger.config;
 import com.github.sparkzxl.swagger.properties.SwaggerProperties;
 import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import com.github.sparkzxl.core.entity.AuthUserInfo;
+import com.google.common.collect.Lists;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Contact;
+import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.List;
+import java.util.Objects;
 
 /**
  * description: Swagger2文档配置
@@ -35,6 +40,9 @@ public class SwaggerAutoConfiguration {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(apiInfo(swaggerProperties))
                 .groupName(swaggerProperties.getGroupName())
+                .globalOperationParameters(
+                        buildGlobalOperationParametersFromSwaggerProperties(
+                                swaggerProperties.getGlobalOperationParameters()))
                 .select()
                 .apis(RequestHandlerSelectors.basePackage(swaggerProperties.getBasePackage()))
                 .paths(PathSelectors.any())
@@ -57,4 +65,30 @@ public class SwaggerAutoConfiguration {
                 .build();
     }
 
+
+    private List<Parameter> buildGlobalOperationParametersFromSwaggerProperties(
+            List<SwaggerProperties.GlobalOperationParameter> globalOperationParameters) {
+        List<Parameter> parameters = Lists.newArrayList();
+
+        if (Objects.isNull(globalOperationParameters)) {
+            return parameters;
+        }
+        for (SwaggerProperties.GlobalOperationParameter globalOperationParameter : globalOperationParameters) {
+            parameters.add(new ParameterBuilder()
+                    .name(globalOperationParameter.getName())
+                    .description(globalOperationParameter.getDescription())
+                    .defaultValue(globalOperationParameter.getDefaultValue())
+                    .required(globalOperationParameter.getRequired())
+                    .allowMultiple(globalOperationParameter.getAllowMultiple())
+                    .parameterType(globalOperationParameter.getParameterType())
+                    .modelRef(new ModelRef(globalOperationParameter.getModelRef()))
+                    .hidden(globalOperationParameter.getHidden())
+                    .pattern(globalOperationParameter.getPattern())
+                    .collectionFormat(globalOperationParameter.getCollectionFormat())
+                    .allowEmptyValue(globalOperationParameter.getAllowEmptyValue())
+                    .order(globalOperationParameter.getOrder())
+                    .build());
+        }
+        return parameters;
+    }
 }
--- sparkzxl-swagger-starter/src/main/java/com/github/sparkzxl/swagger/properties/SwaggerProperties.java
+++ sparkzxl-swagger-starter/src/main/java/com/github/sparkzxl/swagger/properties/SwaggerProperties.java
@@ -1,8 +1,13 @@
 package com.github.sparkzxl.swagger.properties;
 
 
+import io.swagger.annotations.Authorization;
+import io.swagger.annotations.AuthorizationScope;
 import lombok.*;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import springfox.documentation.service.AllowableValues;
+
+import java.util.List;
 
 /**
  * description: swagger属性配置
@@ -25,7 +30,6 @@ public class SwaggerProperties {
     private String description;
 
     private String version;
-
     /**
      * 许可证
      **/
@@ -40,6 +44,10 @@ public class SwaggerProperties {
 
     private Contact contact = new Contact();
 
+    /**
+     * 全局参数配置
+     **/
+    private List<GlobalOperationParameter> globalOperationParameters;
 
     @Data
     @AllArgsConstructor
@@ -59,4 +67,49 @@ public class SwaggerProperties {
         private String email = "";
     }
 
+    @Setter
+    @Getter
+    public static class GlobalOperationParameter {
+        /**
+         * 参数名
+         **/
+        private String name;
+
+        /**
+         * 描述信息
+         **/
+        private String description = "全局参数";
+
+        /**
+         * 指定参数类型
+         **/
+        private String modelRef = "String";
+
+        /**
+         * 参数放在哪个地方:header,query,path,body.form
+         **/
+        private String parameterType = "header";
+
+        /**
+         * 参数是否必须传
+         **/
+        private Boolean required = false;
+        private Boolean allowMultiple = false;
+        private AllowableValues allowableValues;
+        private Boolean hidden = false;
+        private String pattern = "";
+        private String collectionFormat = "";
+        /**
+         * 默认值
+         */
+        private String defaultValue = "";
+        /**
+         * 允许为空
+         */
+        private Boolean allowEmptyValue = true;
+        /**
+         * 排序
+         */
+        private int order = 1;
+    }
 }
