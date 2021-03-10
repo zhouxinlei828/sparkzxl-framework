3b910541d21c4f126a6868d14b0d7d3e397ff237
周鑫磊 <charles.zhou@farben.com.cn>
2021-03-10 18:14:04 +0800

    代码优化

--- sparkzxl-oauth2-open-starter/src/main/java/com/github/sparkzxl/open/component/LogoutSuccessHandlerImpl.java
+++ sparkzxl-oauth2-open-starter/src/main/java/com/github/sparkzxl/open/component/LogoutSuccessHandlerImpl.java
@@ -28,7 +28,7 @@ public class LogoutSuccessHandlerImpl implements LogoutSuccessHandler {
 
     @Override
     public void onLogoutSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Authentication authentication) throws IOException, ServletException {
-        String token = httpServletRequest.getHeader(BaseContextConstants.JWT_TOKEN_HEADER);
+        String token = httpServletRequest.getHeader("token");
         log.info("退出登录成功：{}", token);
         token = StringUtils.removeStartIgnoreCase(token, BaseContextConstants.BEARER_TOKEN);
         if (token != null) {
