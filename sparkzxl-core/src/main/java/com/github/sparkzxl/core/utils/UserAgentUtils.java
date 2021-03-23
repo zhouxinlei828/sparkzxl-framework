0771d20c1e7f6db5224e643be2e9c01833970c86
周鑫磊 <charles.zhou@farben.com.cn>
2021-03-23 15:39:02 +0800

    规范优化

--- sparkzxl-core/src/main/java/com/github/sparkzxl/core/utils/UserAgentUtils.java
+++ sparkzxl-core/src/main/java/com/github/sparkzxl/core/utils/UserAgentUtils.java
@@ -3,10 +3,16 @@ package com.github.sparkzxl.core.utils;
 import cn.hutool.extra.servlet.ServletUtil;
 import cn.hutool.http.useragent.UserAgent;
 import cn.hutool.http.useragent.UserAgentUtil;
+import com.alibaba.fastjson.JSONObject;
 import com.github.sparkzxl.core.entity.UserAgentEntity;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * description: 用户代理工具类
@@ -22,7 +28,14 @@ public class UserAgentUtils extends UserAgentUtil {
         UserAgent userAgent = UserAgentUtil.parse(ua);
         String browserVersion = userAgent.getVersion();
         String clientIp = ServletUtil.getClientIP(request);
-        userAgentEntity.setLocation(AddressUtil.getRegion(clientIp));
+        String region = AddressUtil.getRegion(clientIp);
+        JSONObject locationJsonObj = JSONObject.parseObject(region);
+        String address = locationJsonObj.getString("region");
+        String[] split = StringUtils.split(address, "|");
+        split = ArrayUtils.removeAllOccurrences(split, "0");
+        List<String> areaList = Arrays.stream(split).distinct().collect(Collectors.toList());
+        String clientAddress = StringUtils.join(areaList, " ");
+        userAgentEntity.setLocation(clientAddress);
         userAgentEntity.setRequestIp(clientIp);
         userAgentEntity.setBrowser(userAgent.getBrowser().toString());
         userAgentEntity.setBrowserVersion(browserVersion);
