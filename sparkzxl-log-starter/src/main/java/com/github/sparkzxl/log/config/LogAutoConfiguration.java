0610660eec9b3fbb25afb873e7d2ec04cda5d7fb
zhouxinlei <zhouxinlei298@163.com>
2021-06-05 17:52:50 +0800

    web log日志在线展示

--- sparkzxl-log-starter/src/main/java/com/github/sparkzxl/log/config/LogAutoConfiguration.java
+++ sparkzxl-log-starter/src/main/java/com/github/sparkzxl/log/config/LogAutoConfiguration.java
@@ -1,26 +1,11 @@
 package com.github.sparkzxl.log.config;
 
 import com.github.sparkzxl.log.aspect.WebLogAspect;
-import com.github.sparkzxl.log.netty.NettyServer;
 import com.github.sparkzxl.log.properties.LogProperties;
-import com.github.sparkzxl.log.realtime.FileLogListening;
-import com.github.sparkzxl.log.realtime.LoggerDisruptorQueue;
-import com.github.sparkzxl.log.realtime.ProcessLogFilter;
-import com.github.sparkzxl.log.realtime.disruptor.FileLoggerEventHandler;
-import com.github.sparkzxl.log.realtime.disruptor.LoggerEventHandler;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-
-import java.net.InetAddress;
 
 /**
  * description: 日志增强自动装配
@@ -29,10 +14,9 @@ import java.net.InetAddress;
  * @date 2021-05-23 13:07:55
  */
 @Configuration
-@EnableWebSocketMessageBroker
 @EnableConfigurationProperties(value = {LogProperties.class})
 @Slf4j
-public class LogAutoConfiguration extends AbstractWebSocketMessageBrokerConfigurer {
+public class LogAutoConfiguration {
 
     @Bean
     public WebLogAspect webLogAspect(LogProperties logProperties) {
@@ -40,67 +24,4 @@ public class LogAutoConfiguration extends AbstractWebSocketMessageBrokerConfigur
         webLogAspect.setStorage(logProperties.isStorage());
         return webLogAspect;
     }
-
-    private ApplicationContext applicationContext;
-
-    @Autowired
-    public void setApplicationContext(ApplicationContext applicationContext) {
-        this.applicationContext = applicationContext;
-    }
-
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/websocket")
-                .setAllowedOrigins("*")
-                .addInterceptors()
-                .withSockJS();
-    }
-
-    @Bean
-    public LoggerEventHandler loggerEventHandler(SimpMessagingTemplate messagingTemplate) {
-        LoggerEventHandler loggerEventHandler = new LoggerEventHandler();
-        loggerEventHandler.setMessagingTemplate(messagingTemplate);
-        return loggerEventHandler;
-    }
-
-    @Bean
-    public FileLoggerEventHandler fileLoggerEventHandler(SimpMessagingTemplate messagingTemplate) {
-        FileLoggerEventHandler fileLoggerEventHandler = new FileLoggerEventHandler();
-        fileLoggerEventHandler.setMessagingTemplate(messagingTemplate);
-        return fileLoggerEventHandler;
-    }
-
-    @Bean
-    public LoggerDisruptorQueue loggerDisruptorQueue(LoggerEventHandler loggerEventHandler, FileLoggerEventHandler fileLoggerEventHandler) {
-        return new LoggerDisruptorQueue(loggerEventHandler, fileLoggerEventHandler);
-    }
-
-    @Bean
-    public ProcessLogFilter processLogFilter() {
-        return new ProcessLogFilter();
-    }
-
-    @Bean
-    public FileLogListening fileLogListening(LogProperties logProperties) {
-        FileLogListening fileLogListening = new FileLogListening();
-        fileLogListening.setLogProperties(logProperties);
-        return fileLogListening;
-    }
-
-    @Bean
-    public NettyServer nettyServer() {
-        Environment environment = applicationContext.getEnvironment();
-        try {
-            NettyServer nettyServer = new NettyServer(12345, "/ws");
-            nettyServer.start();
-            log.info("NettyServer 启动成功:{}:{}/{}",
-                    InetAddress.getLocalHost().getHostAddress(),
-                    environment.getProperty("server.port"), "/ws");
-            return nettyServer;
-        } catch (Exception e) {
-            log.error("NettyServerError:{}", e.getMessage());
-        }
-        return null;
-    }
-
 }
--- sparkzxl-log-starter/src/main/java/com/github/sparkzxl/log/config/LoggingWsServer.java
+++ sparkzxl-log-starter/src/main/java/com/github/sparkzxl/log/config/LoggingWsServer.java
@@ -1,13 +1,10 @@
-/*
-package com.github.sparkzxl.log;
+package com.github.sparkzxl.log.config;
 
 
-import com.github.sparkzxl.log.config.MyEndpointConfigure;
-import com.github.sparkzxl.log.realtime.LoggerDisruptorQueue;
+import com.github.sparkzxl.core.utils.StrPool;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.concurrent.CustomizableThreadFactory;
-import org.springframework.stereotype.Component;
 import org.springframework.util.ResourceUtils;
 
 import javax.websocket.*;
@@ -15,8 +12,6 @@ import javax.websocket.server.ServerEndpoint;
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.LinkedBlockingDeque;
@@ -24,42 +19,37 @@ import java.util.concurrent.ThreadPoolExecutor;
 
 import static java.util.concurrent.TimeUnit.NANOSECONDS;
 
-*/
 /**
  * description: WebSocket获取实时日志并输出到Web页面
  *
  * @author zhouxinlei
- * @date 2021-06-05 09:25:03
- *//*
-
+ * @date 2021-06-05 12:31:54
+ */
 @Slf4j
-@Component
 @ServerEndpoint(value = "/websocket/logging", configurator = MyEndpointConfigure.class)
-public class LoggingWSServer {
+public class LoggingWsServer {
 
     @Value("${spring.application.name}")
     private String applicationName;
 
-    */
-/**
+    /**
      * 连接集合
-     *//*
-
+     */
     private static final Map<String, Session> SESSION_MAP = new ConcurrentHashMap<>();
     private static final Map<String, Integer> LENGTH_MAP = new ConcurrentHashMap<>();
 
-    ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(2,
+    ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1,
             3,
             0,
             NANOSECONDS,
             new LinkedBlockingDeque<>(1000),
             new CustomizableThreadFactory());
 
-    */
-/**
+    /**
      * 连接建立成功调用的方法
-     *//*
-
+     *
+     * @param session 连接
+     */
     @OnOpen
     public void onOpen(Session session) {
         //添加到集合中
@@ -71,7 +61,7 @@ public class LoggingWSServer {
         threadPoolExecutor.execute(() -> {
             while (SESSION_MAP.get(session.getId()) != null) {
                 //日志文件路径，获取最新的
-                String logPath = System.getProperty("user.home") + "/log/" + new SimpleDateFormat("yyyyMMdd").format(new Date()) + "/" + applicationName + ".log";
+                String logPath = System.getProperty("user.home") + "/logs/" + applicationName + ".log";
                 try {
                     File logFile = ResourceUtils.getFile(logPath);
                     RandomAccessFile randomFile = new RandomAccessFile(logFile, "rw");
@@ -84,13 +74,16 @@ public class LoggingWSServer {
                             log = log.replaceAll("&", "&amp;")
                                     .replaceAll("<", "&lt;")
                                     .replaceAll(">", "&gt;")
-                                    .replaceAll("\"", "&quot;");
+                                    .replaceAll("\"", "&quot;")
+                                    .replace(" ", StrPool.HTML_NBSP)
+                                    .replace("\tat", StrPool.HTML_NBSP);
 
                             //处理等级
                             log = log.replace("DEBUG", "<span style='color: blue;'>DEBUG</span>");
                             log = log.replace("INFO", "<span style='color: green;'>INFO</span>");
                             log = log.replace("WARN", "<span style='color: orange;'>WARN</span>");
                             log = log.replace("ERROR", "<span style='color: red;'>ERROR</span>");
+                            log = log.replace("application", "<span style='color: #3FB1F5;'>application</span>");
                             //处理类名
                             String[] split = log.split("]");
                             if (split.length >= 2) {
@@ -99,12 +92,9 @@ public class LoggingWSServer {
                                     log = split[0] + "]" + "<span style='color: #298a8a;'>" + split1[0] + "</span>" + "-" + split1[1];
                                 }
                             }
-                            result = result.concat(log).concat("<br/>");
-                            LoggerDisruptorQueue.publishEvent(log);
+                            send(session, log.concat("<br/>"));
                         }
                         LENGTH_MAP.put(session.getId(), (int) randomFile.length());
-                        //发送
-                        send(session, result);
                         //休眠一秒
                         Thread.sleep(1000);
                     } catch (IOException | InterruptedException e) {
@@ -118,11 +108,11 @@ public class LoggingWSServer {
         });
     }
 
-    */
-/**
+    /**
      * 连接关闭调用的方法
-     *//*
-
+     *
+     * @param session
+     */
     @OnClose
     public void onClose(Session session) {
         //从集合中删除
@@ -130,31 +120,34 @@ public class LoggingWSServer {
         LENGTH_MAP.remove(session.getId());
     }
 
-    */
-/**
+    /**
      * 发生错误时调用
-     *//*
-
+     *
+     * @param session
+     * @param error
+     */
     @OnError
     public void onError(Session session, Throwable error) {
         error.printStackTrace();
     }
 
-    */
-/**
+    /**
      * 服务器接收到客户端消息时调用的方法
-     *//*
-
+     *
+     * @param message
+     * @param session
+     */
     @OnMessage
     public void onMessage(String message, Session session) {
 
     }
 
-    */
-/**
+    /**
      * 封装一个send方法，发送消息到前端
-     *//*
-
+     *
+     * @param session
+     * @param message
+     */
     private void send(Session session, String message) {
         try {
             session.getBasicRemote().sendText(message);
@@ -163,4 +156,3 @@ public class LoggingWSServer {
         }
     }
 }
-*/
