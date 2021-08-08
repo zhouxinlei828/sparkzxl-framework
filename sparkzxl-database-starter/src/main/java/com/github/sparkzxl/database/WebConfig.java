283b9949d078d5dc44f0f16a8a3f4145e27bdd70
zhouxinlei <zhouxinlei@cngongbao.com>
2021-08-08 19:57:13 +0800

    代码优化

--- sparkzxl-database-starter/src/main/java/com/github/sparkzxl/database/WebConfig.java
+++ sparkzxl-database-starter/src/main/java/com/github/sparkzxl/database/WebConfig.java
@@ -2,11 +2,12 @@ package com.github.sparkzxl.database;
 
 import com.baomidou.dynamic.datasource.spring.boot.autoconfigure.DynamicDataSourceProperties;
 import com.github.sparkzxl.database.interceptor.DynamicDataSourceInterceptor;
+import com.github.sparkzxl.database.properties.DynamicDataProperties;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ClassUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.ApplicationContext;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -20,16 +21,18 @@ import java.util.Optional;
  * @author zhouxinlei
  */
 @Configuration
+@EnableConfigurationProperties(DynamicDataProperties.class)
 @Slf4j
 public class WebConfig implements WebMvcConfigurer {
 
     public static final String DEFAULT_DYNAMIC_DATASOURCE_INTERCEPTOR_NAME = "dynamicDataSourceInterceptor";
 
-    private ApplicationContext applicationContext;
+
+    private DynamicDataProperties dynamicDataProperties;
 
     @Autowired
-    public void setApplicationContext(ApplicationContext applicationContext) {
-        this.applicationContext = applicationContext;
+    public void setDynamicDataProperties(DynamicDataProperties dynamicDataProperties) {
+        this.dynamicDataProperties = dynamicDataProperties;
     }
 
     @ConditionalOnProperty(prefix = DynamicDataSourceProperties.PREFIX, name = "enabled", havingValue = "true",
@@ -41,9 +44,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        boolean containsBean = applicationContext.containsBean(DEFAULT_DYNAMIC_DATASOURCE_INTERCEPTOR_NAME);
-        if (containsBean) {
-            DynamicDataSourceInterceptor dynamicDataSourceInterceptor = applicationContext.getBean(DynamicDataSourceInterceptor.class);
+        if (dynamicDataProperties.isEnabled()) {
+            DynamicDataSourceInterceptor dynamicDataSourceInterceptor = dynamicDataSourceInterceptor();
             Optional.of(dynamicDataSourceInterceptor).ifPresent(interceptor -> {
                 registry.addInterceptor(interceptor);
                 log.info("已加载拦截器：[{}]", ClassUtils.getName(interceptor));
---
+++ sparkzxl-database-starter/src/main/java/com/github/sparkzxl/database/properties/DynamicDataProperties.java
@@ -0,0 +1,19 @@
+package com.github.sparkzxl.database.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * description: 动态数据源配置类
+ *
+ * @author zhouxinlei
+ * @date 2021-08-08 19:37:40
+ */
+@Data
+@ConfigurationProperties(
+        prefix = com.baomidou.dynamic.datasource.spring.boot.autoconfigure.DynamicDataSourceProperties.PREFIX
+)
+public class DynamicDataProperties {
+
+    private boolean enabled = true;
+}
