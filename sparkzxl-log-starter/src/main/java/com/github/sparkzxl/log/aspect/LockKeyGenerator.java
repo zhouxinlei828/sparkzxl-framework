25778d4e9a9444e7bc2006fdd7207ad7a6e5e989
zhouxinlei <zhouxinlei298@163.com>
2021-11-24 15:16:33 +0800

    security权限控制

--- sparkzxl-log-starter/src/main/java/com/github/sparkzxl/log/aspect/LockKeyGenerator.java
+++ sparkzxl-log-starter/src/main/java/com/github/sparkzxl/log/aspect/LockKeyGenerator.java
@@ -8,12 +8,7 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.AnnotatedElementUtils;
 import org.springframework.core.annotation.AnnotationUtils;
-import org.springframework.util.CollectionUtils;
-import org.springframework.util.ReflectionUtils;
-import org.springframework.util.StringUtils;
 
-import java.lang.annotation.Annotation;
-import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.util.Collections;
