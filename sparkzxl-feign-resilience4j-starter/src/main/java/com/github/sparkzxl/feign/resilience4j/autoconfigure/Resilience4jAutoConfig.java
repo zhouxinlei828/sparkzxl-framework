c90e73b24b6b2588d48cd8fff952fb43b1f68ce1
zhouxinlei <zhouxinlei@cngongbao.com>
2022-04-19 18:28:50 +0800

    Resilience4JCircuitBreakerFactory

--- sparkzxl-feign-resilience4j-starter/src/main/java/com/github/sparkzxl/feign/resilience4j/autoconfigure/Resilience4jAutoConfig.java
+++ sparkzxl-feign-resilience4j-starter/src/main/java/com/github/sparkzxl/feign/resilience4j/autoconfigure/Resilience4jAutoConfig.java
@@ -19,12 +19,6 @@ public class Resilience4jAutoConfig {
 
     @Bean
     public Resilience4JCircuitBreakerFactory resilience4JCircuitBreakerFactory(CircuitBreakerRegistry circuitBreakerRegistry, TimeLimiterRegistry timeLimiterRegistry, Resilience4jBulkheadProvider bulkheadProvider) {
-        Resilience4JCircuitBreakerFactory reactiveResilience4JCircuitBreakerFactory = new Resilience4JCircuitBreakerFactory(circuitBreakerRegistry, timeLimiterRegistry, bulkheadProvider);
-        reactiveResilience4JCircuitBreakerFactory.configure(
-                builder -> builder
-                        .timeLimiterConfig(timeLimiterRegistry.getConfiguration("backendB").orElse(TimeLimiterConfig.custom().timeoutDuration(Duration.ofMillis(300)).build()))
-                        .circuitBreakerConfig(circuitBreakerRegistry.getConfiguration("backendB").orElse(circuitBreakerRegistry.getDefaultConfig())),
-                "backendB");
-        return reactiveResilience4JCircuitBreakerFactory;
+        return new Resilience4JCircuitBreakerFactory(circuitBreakerRegistry, timeLimiterRegistry, bulkheadProvider);
     }
 }
