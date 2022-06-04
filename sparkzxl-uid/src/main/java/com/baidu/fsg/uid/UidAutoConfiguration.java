4e147ee82dd33bc57522ba19847128b4772d250e
zhouxinlei <zhouxinlei298@163.com>
2022-06-04 11:52:15 +0800

    spring boot 2.7.0升级&&spring-cloud升级2021.0.3

---
+++ sparkzxl-uid/src/main/java/com/baidu/fsg/uid/UidAutoConfiguration.java
@@ -0,0 +1,23 @@
+package com.baidu.fsg.uid;
+
+import com.baidu.fsg.uid.worker.DisposableWorkerIdAssigner;
+import com.baidu.fsg.uid.worker.WorkerIdAssigner;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * description: 全局id自动配置
+ *
+ * @author zhouxinlei
+ */
+@Configuration
+public class UidAutoConfiguration {
+
+    @Bean
+    @ConditionalOnMissingBean
+    public WorkerIdAssigner workerIdAssigner() {
+        return new DisposableWorkerIdAssigner((workerNodeEntity) -> {
+        });
+    }
+}
