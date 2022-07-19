30c20d870c21329bb0f653c5091e33ccfcd23fe9
zhouxinlei <zhouxinlei@cngongbao.com>
2022-07-19 15:32:50 +0800

    代码注释

--- sparkzxl-boot-starter/src/main/java/com/github/sparkzxl/boot/BootAutoConfiguration.java
+++ sparkzxl-boot-starter/src/main/java/com/github/sparkzxl/boot/BootAutoConfiguration.java
@@ -1,5 +1,6 @@
 package com.github.sparkzxl.boot;
 
+import com.alibaba.ttl.threadpool.TtlExecutors;
 import com.github.sparkzxl.boot.undertow.UndertowServerFactoryCustomizer;
 import io.undertow.Undertow;
 import lombok.extern.slf4j.Slf4j;
@@ -8,6 +9,11 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * description: boot自动装配
@@ -30,6 +36,12 @@ public class BootAutoConfiguration {
     @ConditionalOnClass(Undertow.class)
     public UndertowServerFactoryCustomizer getUndertowServerFactoryCustomizer() {
         return new UndertowServerFactoryCustomizer();
+    }
+
+    @Bean(name = "threadPoolTaskExecutor")
+    public ExecutorService threadPoolTaskExecutor() {
+        return TtlExecutors.getTtlExecutorService(new ThreadPoolExecutor(50, 300, 300,
+                TimeUnit.SECONDS, new LinkedBlockingQueue<>(), new ThreadPoolExecutor.CallerRunsPolicy()));
     }
 
 }
