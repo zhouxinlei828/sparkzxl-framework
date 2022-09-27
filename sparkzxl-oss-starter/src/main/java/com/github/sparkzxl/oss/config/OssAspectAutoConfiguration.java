a17197c42f5362c79bd6c4836ba9ab226f0dc835
zhouxinlei <zhouxinlei298@163.com>
2022-09-27 15:11:20 +0800

    oss上传

--- sparkzxl-oss-starter/src/main/java/com/github/sparkzxl/oss/config/OssAspectAutoConfiguration.java
+++ sparkzxl-oss-starter/src/main/java/com/github/sparkzxl/oss/config/OssAspectAutoConfiguration.java
@@ -3,6 +3,8 @@ package com.github.sparkzxl.oss.config;
 import com.github.sparkzxl.oss.annotation.OSSUpload;
 import com.github.sparkzxl.oss.aop.OSSUploadAnnotationAdvisor;
 import com.github.sparkzxl.oss.aop.OSSUploadInterceptor;
+import com.github.sparkzxl.oss.listener.DefaultUploadListener;
+import com.github.sparkzxl.oss.listener.UploadListener;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
@@ -23,5 +25,10 @@ public class OssAspectAutoConfiguration {
     @Bean
     public OSSUploadAnnotationAdvisor uploadAnnotationAdvisor(OSSUploadInterceptor uploadInterceptor) {
         return new OSSUploadAnnotationAdvisor(uploadInterceptor, OSSUpload.class, Ordered.HIGHEST_PRECEDENCE);
+    }
+
+    @Bean
+    public UploadListener defaultUploadListener() {
+        return new DefaultUploadListener();
     }
 }
