db2c6d97b8f12795fc422696fbede6352d373f64
zhouxinlei <zhouxinlei298@163.com>
2022-12-06 13:54:14 +0800

    NacosWatch

--- sparkzxl-distributed-cloud-starter/src/main/java/com/github/sparkzxl/distributed/cloud/config/NacosConfig.java
+++ sparkzxl-distributed-cloud-starter/src/main/java/com/github/sparkzxl/distributed/cloud/config/NacosConfig.java
@@ -2,10 +2,13 @@ package com.github.sparkzxl.distributed.cloud.config;
 
 import cn.hutool.core.date.DateUtil;
 import com.alibaba.cloud.nacos.NacosDiscoveryProperties;
+import com.alibaba.cloud.nacos.NacosServiceManager;
+import com.alibaba.cloud.nacos.discovery.NacosWatch;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.Map;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
 /**
  * description:
@@ -17,11 +20,11 @@ import java.util.Map;
 public class NacosConfig {
 
     @Bean
-    public NacosDiscoveryProperties nacosProperties() {
-        NacosDiscoveryProperties nacosDiscoveryProperties = new NacosDiscoveryProperties();
-        Map<String, String> metadata = nacosDiscoveryProperties.getMetadata();
-        metadata.put("startup.time", DateUtil.now());
-        return nacosDiscoveryProperties;
+    @ConditionalOnProperty(value = {"spring.cloud.nacos.discovery.watch.enabled"}, matchIfMissing = true)
+    public NacosWatch nacosWatch(NacosServiceManager nacosServiceManager, NacosDiscoveryProperties nacosDiscoveryProperties, ObjectProvider<ThreadPoolTaskScheduler> taskScheduler) {
+        //更改服务详情中的元数据，增加服务注册时间
+        nacosDiscoveryProperties.getMetadata().put("startup.time", DateUtil.now());
+        return new NacosWatch(nacosServiceManager, nacosDiscoveryProperties, taskScheduler);
     }
 
 }
