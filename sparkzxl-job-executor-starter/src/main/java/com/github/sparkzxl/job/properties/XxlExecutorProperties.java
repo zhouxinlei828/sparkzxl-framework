90e3a4b316b056c29ff5b58eb6efedd2518c8b95
zhouxinlei <zhouxinlei@cngongbao.com>
2025-01-24 10:21:29 +0800

    XXL-JOB代码更新

--- sparkzxl-job-executor-starter/src/main/java/com/github/sparkzxl/job/properties/XxlExecutorProperties.java
+++ sparkzxl-job-executor-starter/src/main/java/com/github/sparkzxl/job/properties/XxlExecutorProperties.java
@@ -1,9 +1,9 @@
 package com.github.sparkzxl.job.properties;
 
-import static com.github.sparkzxl.job.properties.XxlExecutorProperties.XXL_JOB_PREFIX;
-
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import static com.github.sparkzxl.job.properties.XxlExecutorProperties.XXL_JOB_PREFIX;
 
 /**
  * description: xxl job属性配置类
@@ -32,7 +32,7 @@ public class XxlExecutorProperties {
     /**
      * 日志路径
      */
-    private String logPath;
+    private String logPath = System.getProperty("user.home") + "/logs/xxl-job";
 
     /**
      * 日志保留天数
