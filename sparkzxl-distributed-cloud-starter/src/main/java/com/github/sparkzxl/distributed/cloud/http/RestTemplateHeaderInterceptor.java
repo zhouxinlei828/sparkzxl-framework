eff4bb5bc460528becf12118316f89c626bda5cc
zhouxinlei <zhouxinlei298@163.com>
2025-02-27 18:50:55 +0800

    代码更新

--- sparkzxl-distributed-cloud-starter/src/main/java/com/github/sparkzxl/distributed/cloud/http/RestTemplateHeaderInterceptor.java
+++ sparkzxl-distributed-cloud-starter/src/main/java/com/github/sparkzxl/distributed/cloud/http/RestTemplateHeaderInterceptor.java
@@ -51,7 +51,7 @@ public class RestTemplateHeaderInterceptor implements ClientHttpRequestIntercept
         }
 
         HttpServletRequest httpServletRequest = ((ServletRequestAttributes) requestAttributes).getRequest();
-        if (request == null) {
+        if (httpServletRequest == null) {
             log.warn("path={}, 在FeignClient API接口未配置FeignConfiguration类， 故而无法在远程调用时获取请求头中的参数!", request.getURI());
             return execution.execute(request, bytes);
         }
